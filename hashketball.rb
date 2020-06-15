f49a9b2c1f4c9496d9a1fe9a1e8a999ef9f5fc0a
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-15 11:48:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,29 +4,22 @@ require 'pry'
 
 def game_hash
   {
-    :home => {}
-    :away => {}
+    :home => {
+      :team_name => ""
+      :colors => [""]
+      :players => [{
+        :player_name =>
+        :number =>
+        :shoe => 
+      }]
+    },
+    :away => {
+      
+    }
   }
 end
 
-
-
-
-* The top level of the hash has two keys: `:home`, for the home team, and
-  `:away`, for the away team.
-* The values of the `:home` and `:away` keys are hashes. These hashes have the
-  following keys:
-  * `:team_name`
-  * `:colors`
-  * `:players`
-* The `:team_name` key points to a string of the team name.
-* The `:colors` key points to an array of strings that are that team's colors.
-* The `:players` key points to an `Array` of `Hash`es. Each `Hash` in the `Array`
-  should contain the players' stats. Each stat should be a key that points to
-  a value for that stat. The keys should be:
-  * `:player_name`
-  * `:number`
-  * `:shoe`
+  *
   * `:points`
   * `:rebounds`
   * `:assists`
