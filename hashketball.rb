fec10a008a0aaa13a5222dd364e657e1f472a930
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-15 12:14:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -103,7 +103,7 @@ def game_hash
           :assists => 2, 
           :steals => 1, 
           :blocks => 1, 
-          :slam_dunks => },
+          :slam_dunks => 0 },
         
         { :player_name =>
           :number =>
