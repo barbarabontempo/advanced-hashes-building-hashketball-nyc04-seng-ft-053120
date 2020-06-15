bba325bdd4b84c5f592e43b420cc2239f48e901a
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-15 11:43:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,56 @@
 # Write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+There is one method in this lab to implement: `game_hash`. This method contains and
+returns a hash nested in the following manner:
+
+* The top level of the hash has two keys: `:home`, for the home team, and
+  `:away`, for the away team.
+* The values of the `:home` and `:away` keys are hashes. These hashes have the
+  following keys:
+  * `:team_name`
+  * `:colors`
+  * `:players`
+* The `:team_name` key points to a string of the team name.
+* The `:colors` key points to an array of strings that are that team's colors.
+* The `:players` key points to an `Array` of `Hash`es. Each `Hash` in the `Array`
+  should contain the players' stats. Each stat should be a key that points to
+  a value for that stat. The keys should be:
+  * `:player_name`
+  * `:number`
+  * `:shoe`
+  * `:points`
+  * `:rebounds`
+  * `:assists`
+  * `:steals`
+  * `:blocks`
+  * `:slam_dunks`
+* The data for each player is provided in a table below.
+
+Home Team:
+
+* team name: Brooklyn Nets
+* colors: Black, White
+* players:
+
+|          Stat          | Info | Info |  Info | Info | Info   |
+|:------------------:|:-------------:|:------------:|:------------:|:-------------:|:-------------:|
+| **Player Name**    |  Alan Anderson| Reggie Evans | Brook Lopez  | Mason Plumlee | Jason Terry   |
+| **Number**         | 0             | 30           | 11           | 1             | 31            |
+| **Shoe**           | 16            | 14           | 17           | 19            | 15            |
+| **Points**         | 22            | 12           | 17           | 26            | 19            |
+| **Rebounds**       | 12            | 12           | 19           | 11            | 2             |
+| **Assists**        | 12            | 12           | 10           | 6             | 2             |
+| **Steals**         | 3             | 12           | 3            | 3             | 4             |
+| **Blocks**         | 1             | 12           | 1            | 8             | 11            |
+| **Slam Dunks**     | 1             | 7            | 15           | 5             | 1             |
