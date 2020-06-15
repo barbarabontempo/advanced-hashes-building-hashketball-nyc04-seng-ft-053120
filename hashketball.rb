879bdbf031c0395e426a7dc469b7d614c0e4ef02
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-15 12:10:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,8 +66,8 @@ def game_hash
       :players => [
         
         { :player_name => "Jeff Adrien",
-          :number => 0,
-          :shoe => 16,
+          :number => 4,
+          :shoe => 18,
           :points => 22,
           :rebounds => 12, 
           :assists => 12, 
